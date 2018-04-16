350be33285cc5e816b228e5928024d3554d9aa28
Shaival Chokshi <shaival@amazon.com>
2018-04-17 01:24:43 +0530

    Changes in test file

--- Algorithm Practice/Stack/Test.cpp
+++ Algorithm Practice/Stack/Test.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+void dummyMethod()
+{
+	cout<<"Welcome to Coding\n";
+}
+
 int main()
 {
-    cout <<"Hello World";
+    cout <<"Hello World\n";
+    dummyMethod();
     return 0;
 }
