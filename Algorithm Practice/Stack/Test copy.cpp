cba1b0900c0d85f8299f14bcbcfebfbcc305e37d
Shaival Chokshi <shaival@amazon.com>
2018-04-17 01:31:30 +0530

    Adding test copy

---
+++ Algorithm Practice/Stack/Test copy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void dummyMethod()
+{
+	cout<<"Welcome to Coding - Shaival\n";
+}
+
+int main()
+{
+    cout <<"Hello World\n";
+    dummyMethod();
+    return 0;
+}
